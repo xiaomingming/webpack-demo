7bbd6d8dc55dc3c7312a30e521b2eb46aafd4241
leweiming <leweiming@alibaba-inc.com>
2017-05-25 15:43:45 +0800

    增加nodeEnv变量测试

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 function buildConfig(env) {
-  // console.log('env is:' + env);
-  return require('./webpack.' + env + '.js')({
-    env: env
-  })
+  console.log(`env is: ${env}`);
+  console.log(`process env is: ${process.env.NODE_ENV}`);
+  var file = require('./webpack.' + env + '.js');
+  console.log(file);
+  return require('./webpack.' + env + '.js')
 }
 module.exports = buildConfig;
